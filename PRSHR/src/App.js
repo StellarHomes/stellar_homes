e14f0428bb6fc9a05954c296ac4965107ac9a030
Juan Huerfano <165720974+Juandxh@users.noreply.github.com>
2025-04-23 11:19:04 -0500

    rutas

--- PRSHR/src/App.js
+++ PRSHR/src/App.js
@@ -30,7 +30,7 @@ function App() {
           <Route path="/inmueble" element={<InmueblesListUsu />} />
           <Route path="/editarInmueble/:idInmueble" element={<EditarInmueble />} />
           <Route path="/IfoInmueble/:id" element={<IfoInmueble />} />
-          <Route path="/Restablecer_Contraseña" element={<Restablecer_Contra />} />
+          <Route path="/restablecer contraseña" element={<Restablecer_Contra />} />
           <Route path="/perfilUsu" element={<PerfilUsu />} />
         </Routes>
       </div>
